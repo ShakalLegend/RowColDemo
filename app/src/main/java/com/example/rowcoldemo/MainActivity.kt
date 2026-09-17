2709b50f562cf3f971d6321465acdd9c3cda3d1b
ShakalLegend <shakal228mc@gmail.com>
2026-09-17 11:11:13 +0700

    Align by

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,7 +45,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Row {
-        Text("LargeText", Modifier.alignByBaseline(), fontSize = 40.sp, fontWeight = FontWeight.Bold
+        Text("LargeText\n\nMore Text", Modifier.alignBy(LastBaseline), fontSize = 40.sp, fontWeight = FontWeight.Bold
         )
         Text("SmallText", Modifier.alignByBaseline(), fontSize = 32.sp, fontWeight = FontWeight.Bold
         )
